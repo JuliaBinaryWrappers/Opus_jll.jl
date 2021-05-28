a785fd299778fb37cd2c4acf792de42c9c28175c
jlbuild <juliabuildbot@gmail.com>
2021-05-28 22:45:33 +0000

    Opus_jll build 1.3.2+0

--- src/wrappers/i686-w64-mingw32.jl
+++ src/wrappers/i686-w64-mingw32.jl
@@ -7,7 +7,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libopus,
-        "bin/libopus-0.dll",
+        "bin\\libopus-0.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
